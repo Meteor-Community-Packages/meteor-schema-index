6ff119b03f5f2ed2b133ca9405112e7e6971d1da
Eric Dobbertin <eric@dairystatedesigns.com>
2020-12-09 15:30:38 -0600

    chore: dependency updates

--- package/indexing/package.js
+++ package/indexing/package.js
@@ -1,17 +1,14 @@
 Package.describe({
-  name: 'aldeed:schema-index',
-  summary: 'Control some MongoDB indexing with schema options',
-  version: '3.0.0',
-  documentation: '../../README.md',
-  git: 'https://github.com/aldeed/meteor-schema-index.git',
+  name: "aldeed:schema-index",
+  summary: "Control some MongoDB indexing with schema options",
+  version: "3.0.0",
+  documentation: "../../README.md",
+  git: "https://github.com/aldeed/meteor-schema-index.git",
 });
 
-Package.onUse(function(api) {
-  api.use([
-    'aldeed:collection2@3.0.0',
-    'ecmascript@0.6.1',
-  ]);
+Package.onUse(function (api) {
+  api.use(["aldeed:collection2@3.0.0", "ecmascript@0.6.1"]);
 
-  api.mainModule('client.js', 'client');
-  api.mainModule('server.js', 'server');
+  api.mainModule("client.js", "client");
+  api.mainModule("server.js", "server");
 });
