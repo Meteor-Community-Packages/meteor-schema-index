167fc9b98851860e3fb41fd0cc956244652f2c4b
Jan Dvorak <storytellercz@proton.me>
2024-04-25 13:50:32 +0200

    Update dependencies and compat versions

--- package/indexing/package.js
+++ package/indexing/package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'aldeed:schema-index',
   summary: 'Control some MongoDB indexing with schema options',
-  version: '4.0.0-beta.2',
+  version: '4.0.0-beta.3',
   documentation: '../../README.md',
   git: 'https://github.com/aldeed/meteor-schema-index.git',
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom(["2.8.1", '3.0-beta.4']);
+  api.versionsFrom(["2.8.1", '3.0-rc.0']);
   api.use("ecmascript");
   api.use([
-    'aldeed:collection2@4.0.0',
-    'aldeed:simple-schema@1.13.1',
+    'aldeed:collection2@4.0.1',
+    'aldeed:simple-schema@1.13.1 || 2.0.0-beta300.0',
   ]);
 
   api.mainModule("client.js", "client");
